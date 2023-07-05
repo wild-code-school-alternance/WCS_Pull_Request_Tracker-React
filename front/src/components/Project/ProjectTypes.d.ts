1746a8870e9e34d46fe0b09d4ab7669db0cdbfc4
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-07-05 10:24:47 +0200

    add ProjectTypes.d.ts

---
+++ front/src/components/Project/ProjectTypes.d.ts
@@ -0,0 +1,7 @@
+export type ProjectType = {
+    __typename?: 'Project'
+    id: number;
+    name: string;
+    githubLink: string;
+    lastPR: string;
+};
--- front/src/components/Project/Projects.tsx
+++ front/src/components/Project/Projects.tsx
@@ -1,15 +1,9 @@
 import { useState, useEffect } from 'react';
 import styles from './ProjectCard.module.scss';
-
-interface Project {
-    id: number;
-    name: string;
-    githubLink: string;
-    lastPR: string;
-}
+import {ProjectType} from './ProjectTypes';
 
 function Projects() {
-    const [projects, setProjects] = useState<Project[]>([]);
+    const [projects, setProjects] = useState<ProjectType[]>([]);
 
     useEffect(() => {
         fetchProjects();
@@ -21,24 +15,26 @@ function Projects() {
             if (!response.ok) {
                 throw new Error('OOPS something wrong!')
             }
-            const data: Project[] = await response.json();
+            const data: ProjectType[] = await response.json();
             setProjects(data);
         } catch (error) {
             console.error(error);
         }
     }
 
+
+
     return (
         <div className={`${styles['main-container-fluid']} container-fluid`}>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-                {projects.map((project) => (
-                    <div key={project.id} className="col">
+                {projects.map((Project: ProjectType) => (
+                    <div key={Project.id} className="col">
                         <div className={`card h-100 ${styles.card}`}>
                             <div className={`card-body d-flex flex-column justify-content-between ${styles['card-body']}`}>
-                                <h5 className={`card-title text-white ${styles['card-title']}`}>{project.name}</h5>
-                                <h6 className={`card-subtitle mb-2 text-white ${styles['card-subtitle']}`}>Last PR: {project.lastPR ? project.lastPR : 'Pas de PR pour ce projet.'}</h6>
+                                <h5 className={`card-title text-white ${styles['card-title']}`}>{Project.name}</h5>
+                                <h6 className={`card-subtitle mb-2 text-white ${styles['card-subtitle']}`}>Last PR: {Project.lastPR ? Project.lastPR : 'Pas de PR pour ce projet.'}</h6>
                                 <a href="#" className={`btn btn-primary ${styles.btn}`}>Project Detail </a>
-                                <a href={project.githubLink} target={'_blank'} className={`btn btn-primary ${styles.btn}`}> Github</a>
+                                <a href={Project.githubLink} target={'_blank'} className={`btn btn-primary ${styles.btn}`}> Github</a>
                             </div>
                         </div>
                     </div>
